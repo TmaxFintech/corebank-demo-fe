3dcbc8666deef9977cfac06ee8729db9f9a3321f
hi6724 <hi6724@gmail.com>
2024-06-25 14:49:01 +0900

    ✨ opacity값 지정

---
+++ src/styles/constants/opacity.ts
@@ -0,0 +1,18 @@
+const opacity = {
+  opacity1000: '100%',
+  opacity970: '97%',
+  opacity880: '88%',
+  opacity740: '74%',
+  opacity610: '61%',
+  opacity520: '52%',
+  opacity430: '43%',
+  opacity350: '35%',
+  opacity280: '28%',
+  opacity220: '22%',
+  opacity160: '16%',
+  opacity120: '12%',
+  opacity80: '8%',
+  opacity40: '4%',
+  opacity0: '0%',
+};
+export default { ...opacity };
