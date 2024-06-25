e57bfc0d10b86662ae5175d61b8a92b1cd283239
hi6724 <hi6724@gmail.com>
2024-06-25 14:59:03 +0900

    ✨ 예시코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import './styles/global.css';
 import theme from './styles/theme';
+import Test from './components/Test';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>HELLO WORLD</div>
+      <Test />
     </ThemeProvider>
   );
 }
---
+++ src/components/Test.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+function Test() {
+  return (
+    <div>
+      <STestHello hello={'world'}>T-MAX</STestHello>
+    </div>
+  );
+}
+
+export default Test;
+
+const STestHello = styled.div<{ hello: string }>(({ theme, hello }) => ({
+  ...theme.font.body1,
+  fontWeight: theme.font.semibold,
+  boxShadow: theme.elevations.level3,
+  position: 'relative',
+  padding: '1rem',
+  ...(hello === 'world'
+    ? { color: theme.color.textStrongWhite, background: theme.color.primaryGradation }
+    : { color: theme.color.blue40, background: 'lightgray' }),
+}));
