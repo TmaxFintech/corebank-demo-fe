8b746f63c741ba832180475fa658b8b1065c983f
hi6724 <hi6724@gmail.com>
2024-06-25 14:49:23 +0900

    ✨ spacing값 지정

---
+++ src/styles/constants/spacing.ts
@@ -0,0 +1,17 @@
+const spacing = {
+  spacing0: '0px',
+  spacing20: '2px',
+  spacing40: '4px',
+  spacing80: '8px',
+  spacing120: '12px',
+  spacing160: '16px',
+  spacing200: '20px',
+  spacing240: '24px',
+  spacing280: '28px',
+  spacing320: '32px',
+  spacing360: '36px',
+  spacing400: '40px',
+  spacing480: '48px',
+};
+
+export default { ...spacing };
