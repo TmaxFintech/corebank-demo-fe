6d670b079f88752c6854b9a93a32df614e066daa
LucetTin5 <lucettin5@gmail.com>
2024-06-28 16:56:18 +0900

    feat: icon16, icon24 import/export 설정

---
+++ src/assets/icons/icon16/Icon16Assets.ts
@@ -0,0 +1,59 @@
+import Add from './Add.svg?react';
+import ArrowDownSolid from './ArrowDownSolid.svg?react';
+import Calculator from './Calculator.svg?react';
+import Calendar from './Calendar.svg?react';
+import CheckDark from './CheckDark.svg?react';
+import Clear from './Clear.svg?react';
+import Close from './Close.svg?react';
+import Complete from './Complete.svg?react';
+import DownArrow from './DownArrow.svg?react';
+import DownArrowDark from './DownArrowDark.svg?react';
+import Download from './Download.svg?react';
+import Error from './Error.svg?react';
+import Filter from './Filter.svg?react';
+import IconGuide16 from './IconGuide16.svg?react';
+import InputEssential from './InputEssential.svg?react';
+import Kebab from './Kebab.svg?react';
+import LeftArrow from './LeftArrow.svg?react';
+import Loading from './Loading.svg?react';
+import Pen from './Pen.svg?react';
+import RightArrow from './RightArrow.svg?react';
+import Search from './Search.svg?react';
+import Swap from './Swap.svg?react';
+import Trash from './Trash.svg?react';
+import TwoLeftArrow from './TwoLeftArrow.svg?react';
+import TwoRightArrow from './TwoRightArrow.svg?react';
+import UpArrow from './UpArrow.svg?react';
+
+const Icon16Assets = {
+  Add,
+  ArrowDownSolid,
+  Calculator,
+  Calendar,
+  CheckDark,
+  Clear,
+  Close,
+  Complete,
+  DownArrow,
+  DownArrowDark,
+  Download,
+  Error,
+  Filter,
+  IconGuide16,
+  InputEssential,
+  Kebab,
+  LeftArrow,
+  Loading,
+  Pen,
+  RightArrow,
+  Search,
+  Swap,
+  Trash,
+  TwoLeftArrow,
+  TwoRightArrow,
+  UpArrow,
+};
+
+export type Icon16AssetKey = keyof typeof Icon16Assets;
+
+export default Icon16Assets;
---
+++ src/assets/icons/icon24/Icon24Assets.ts
@@ -0,0 +1,99 @@
+import Add from './Add.svg?react';
+import Bell from './Bell.svg?react';
+import Calculator from './Calculator.svg?react';
+import Calendar from './Calendar.svg?react';
+import Check from './Check.svg?react';
+import Clear from './Clear.svg?react';
+import ClearDark from './ClearDark.svg?react';
+import Close from './Close.svg?react';
+import Complete from './Complete.svg?react';
+import Copy from './Copy.svg?react';
+import CurvedArrow from './CurvedArrow.svg?react';
+import CustomerInformation from './CustomerInformation.svg?react';
+import DownArrow from './DownArrow.svg?react';
+import DownArrowSolid from './DownArrowSolid.svg?react';
+import Download from './Download.svg?react';
+import Error from './Error.svg?react';
+import Export from './Export.svg?react';
+import Filter from './Filter.svg?react';
+import Home from './Home.svg?react';
+import IconGuide24 from './IconGuide24.svg?react';
+import LNBDownArrow from './LNBDownArrow.svg?react';
+import LNBUpArrow from './LNBUpArrow.svg?react';
+import LeftArrow from './LeftArrow.svg?react';
+import Loading from './Loading.svg?react';
+import Loading1 from './Loading1.svg?react';
+import Loading2 from './Loading2.svg?react';
+import Loading3 from './Loading3.svg?react';
+import Loading4 from './Loading4.svg?react';
+import Paper from './Paper.svg?react';
+import Pen from './Pen.svg?react';
+import Print from './Print.svg?react';
+import ProductDescription from './ProductDescription.svg?react';
+import Refresh from './Refresh.svg?react';
+import RightArrow from './RightArrow.svg?react';
+import Scan from './Scan.svg?react';
+import Search from './Search.svg?react';
+import Star from './Star.svg?react';
+import StarFilled from './StarFilled.svg?react';
+import Swap from './Swap.svg?react';
+import Template from './Template.svg?react';
+import Trash from './Trash.svg?react';
+import TwoLeftArrow from './TwoLeftArrow.svg?react';
+import TwoRightArrow from './TwoRightArrow.svg?react';
+import UpArrow from './UpArrow.svg?react';
+import UpArrowSolid from './UpArrowSolid.svg?react';
+import Upload from './Upload.svg?react';
+
+const Icon24Assets = {
+  Add,
+  Bell,
+  Calculator,
+  Calendar,
+  Check,
+  Clear,
+  ClearDark,
+  Close,
+  Complete,
+  Copy,
+  CurvedArrow,
+  CustomerInformation,
+  DownArrow,
+  DownArrowSolid,
+  Download,
+  Error,
+  Export,
+  Filter,
+  Home,
+  IconGuide24,
+  LNBDownArrow,
+  LNBUpArrow,
+  LeftArrow,
+  Loading,
+  Loading1,
+  Loading2,
+  Loading3,
+  Loading4,
+  Paper,
+  Pen,
+  Print,
+  ProductDescription,
+  Refresh,
+  RightArrow,
+  Scan,
+  Search,
+  Star,
+  StarFilled,
+  Swap,
+  Template,
+  Trash,
+  TwoLeftArrow,
+  TwoRightArrow,
+  UpArrow,
+  UpArrowSolid,
+  Upload,
+};
+
+export type Icon24AssetKey = keyof typeof Icon24Assets;
+
+export default Icon24Assets;
