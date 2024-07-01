92a02df7bd6e866fc49e8e4b7269c67da7999312
LucetTin5 <lucettin5@gmail.com>
2024-07-01 10:38:47 +0900

    chore: icon examples

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { ThemeProvider } from 'styled-components';
 import './styles/global.css';
 import theme from './styles/theme';
 import Test from './components/Test';
+import { Icon16, Icon24 } from './shared/components/Icon';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Test />
+      <div>
+        <Icon24 iconName="Add" />
+        <Icon16 iconName="Download" />
+      </div>
     </ThemeProvider>
   );
 }
