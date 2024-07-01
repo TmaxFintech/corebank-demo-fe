b11026265dd5e9813591191285d8add87f75bedd
LucetTin5 <lucettin5@gmail.com>
2024-07-01 13:11:28 +0900

    refactor: 현재 불필요한 wrapper div 제거

--- src/shared/components/Icon.tsx
+++ src/shared/components/Icon.tsx
@@ -1,53 +1,30 @@
-import React, { type HTMLAttributes } from 'react';
-import styled from 'styled-components';
+import type { SVGProps } from 'react';
 import Icon24Assets, {
   type Icon24AssetKey,
-} from '../../assets/icons/icon24/Icon24Assets';
+} from '../../assets/icons/icon24/icon24Assets';
 import Icon16Assets, {
   type Icon16AssetKey,
-} from '../../assets/icons/icon16/Icon16Assets';
+} from '../../assets/icons/icon16/icon16Assets';
 
-type IconSize = '24' | '16';
-
-interface IconBaseProps extends HTMLAttributes<HTMLDivElement> {
-  size: IconSize;
-  icon: React.ReactNode;
-}
-
-function IconBase({ size, icon, ...rest }: IconBaseProps) {
-  return (
-    <StyledIconBase size={size} {...rest}>
-      {icon}
-    </StyledIconBase>
-  );
-}
-
-const StyledIconBase = styled.div<Pick<IconBaseProps, 'size'>>`
-  width: ${({ size }) => `${size}px`};
-  height: ${({ size }) => `${size}px`};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+// IconBase로의 확장 대비
+interface IconBaseProps extends SVGProps<SVGSVGElement> {}
 
 // 나중에 Styled(Icon24)와 같이 사용할 수 있도록 export
-export interface Icon24Props extends Omit<IconBaseProps, 'size' | 'icon'> {
+export interface Icon24Props extends IconBaseProps {
   iconName: Icon24AssetKey;
 }
 
-function Icon24({ iconName, ...rest }: Icon24Props) {
+export function Icon24({ iconName, ...rest }: Icon24Props) {
   const Icon = Icon24Assets[iconName];
-  return <IconBase {...rest} size="24" icon={<Icon />} />;
+  return <Icon {...rest} width="24  " height="24" />;
 }
 
 // 나중에 Styled(Icon16)와 같이 사용할 수 있도록 export
-export interface Icon16Props extends Omit<IconBaseProps, 'size' | 'icon'> {
+export interface Icon16Props extends IconBaseProps {
   iconName: Icon16AssetKey;
 }
 
-function Icon16({ iconName, ...rest }: Icon16Props) {
+export function Icon16({ iconName, ...rest }: Icon16Props) {
   const Icon = Icon16Assets[iconName];
-  return <IconBase {...rest} size="16" icon={<Icon />} />;
+  return <Icon {...rest} width="16" height="16" />;
 }
-
-export { Icon16, Icon24 };
