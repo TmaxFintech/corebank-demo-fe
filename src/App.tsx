85e184427f7f0a88dd6991076de39fe6e24f57ae
LucetTin5 <lucettin5@gmail.com>
2024-07-01 12:14:18 +0900

    feat: notification icon examples

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/global.css';
 import theme from './styles/theme';
 import Test from './components/Test';
 import { Icon16, Icon24 } from './shared/components/Icon';
+import NotificationIcon from './shared/components/NotificationIcon';
 
 function App() {
   return (
@@ -11,6 +12,17 @@ function App() {
       <div>
         <Icon24 iconName="Add" />
         <Icon16 iconName="Download" />
+      </div>
+      <div>
+        <NotificationIcon size="24" type="default" />
+        <NotificationIcon size="24" type="error" />
+        <NotificationIcon size="24" type="success" />
+        <NotificationIcon size="24" type="caution" />
+        <NotificationIcon size="24" type="information" />
+        <NotificationIcon size="16" type="error" />
+        <NotificationIcon size="16" type="success" />
+        <NotificationIcon size="16" type="caution" />
+        <NotificationIcon size="16" type="information" />
       </div>
     </ThemeProvider>
   );
